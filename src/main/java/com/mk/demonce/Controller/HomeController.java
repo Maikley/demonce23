109081b3665974d6c58b09fd93fb49be50f560ff
Maikley <44551136+Maikley@users.noreply.github.com>
2019-02-10 10:45:13 +0300

    Second step add new books

--- src/main/java/com/mk/demonce/Controller/HomeController.java
+++ src/main/java/com/mk/demonce/Controller/HomeController.java
@@ -6,6 +6,8 @@ import com.mk.demonce.repo.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,10 +19,18 @@ public class HomeController {
 
     private BookRepository bookRepository;
 
-@RequestMapping("/")
+@GetMapping
 public String getIndex(Model model){
     model.addAttribute("books",bookRepository.findAll());
+    model.addAttribute("newbook", new Book());
             return "index";
 }
+@PostMapping
+    public String createBook(Book book){
+    bookRepository.save(book);
+    return "redirect:/";
+}
+
+
 
 }
